89cfb48fffaa90dfbe0ffe7004acb390f2b0e2b2
Emin Kabacaoglu <eminkabacaoglu@gmail.com>
2022-05-09 17:39:55 +0300

    Add files via upload

---
+++ NotOrtalamasi.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class NotOrtalamasi {
+    public static void main(String[] args) {
+
+        int matematikNot,fizikNot,kimyaNot,turkceNot,tarihNot,muzikNot;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Matematik Notunu Giriniz: ");
+        matematikNot = input.nextInt();
+
+        System.out.print("Fizik Notunu Giriniz: ");
+        fizikNot = input.nextInt();
+
+        System.out.print("Kimya Notunu Giriniz: ");
+        kimyaNot = input.nextInt();
+
+        System.out.print("Türkçe Notunu Giriniz: ");
+        turkceNot = input.nextInt();
+
+        System.out.print("Tarih Notunu Giriniz: ");
+        tarihNot = input.nextInt();
+
+        System.out.print("Müzik Notunu Giriniz: ");
+        muzikNot = input.nextInt();
+
+        int notToplam = matematikNot + fizikNot + kimyaNot + turkceNot + tarihNot + muzikNot;
+        float notOrtalama = notToplam / 6f; // ya da 6.0 da diyebilirdik
+        // double notOrtalama = notToplam / 6.0; //şeklinde de yazabilirdik
+        String durum = notOrtalama >60 ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+
+        System.out.println("Ortalamanız: "+notOrtalama + "-->" +durum);
+
+
+    }
+}
